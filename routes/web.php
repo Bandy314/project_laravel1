37470c1b1b0b4ed3f4000963a5d23963eea7ea0f
Bandy314 <sanya.borodin2002@bk.ru>
2022-06-17 16:27:09 +0500

    first_commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/products/add', [ProductController::class, 'create'])->name('products.add');
